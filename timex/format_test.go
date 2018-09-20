49c3e2850561cff2c9a715310c8b21dd772e19cf
venjiang <venjiang@gmail.com>
2018-09-20 20:41:42 +0800

    time pretty

--- timex/format_test.go
+++ timex/format_test.go
@@ -1,6 +1,7 @@
 package timex
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -14,4 +15,82 @@ func TestFormat(t *testing.T) {
 	assert.Equal(t, "2018-06-30", Format(a, DateFormat), "they should be equal")
 	assert.Equal(t, "-", Format(time.Time{}, DateFormat, "-"), "they should be equal")
 	// assert.Nil(t, object)
+}
+
+func TestPretty(t *testing.T) {
+	tests := []struct {
+		name string
+		t    time.Time
+		want string
+	}{
+		{name: "Just now", t: time.Now(), want: "刚刚"},
+		{name: "Second", t: time.Now().Add(
+			time.Hour*time.Duration(0) +
+				time.Minute*time.Duration(0) +
+				time.Second*time.Duration(2)),
+			want: "2秒后",
+		},
+		{name: "SecondAgo", t: time.Now().Add(
+			time.Hour*time.Duration(0) +
+				time.Minute*time.Duration(0) +
+				time.Second*time.Duration(-1)),
+			want: "1秒前"},
+		{name: "Minutes", t: time.Now().Add(time.Hour*time.Duration(0) +
+			time.Minute*time.Duration(59) +
+			time.Second*time.Duration(59)), want: "60分钟后"},
+		{name: "Tomorrow", t: time.Now().AddDate(0, 0, 1), want: "明天"},
+		{name: "Yesterday", t: time.Now().AddDate(0, 0, -1), want: "昨天"},
+		{name: "2day", t: time.Now().AddDate(0, 0, 2), want: "2天后"},
+		{name: "2dayAgo", t: time.Now().AddDate(0, 0, -2), want: "2天前"},
+		{name: "Week", t: time.Now().AddDate(0, 0, 7), want: Format(time.Now().AddDate(0, 0, 7))},
+		{name: "WeekAgo", t: time.Now().AddDate(0, 0, -7), want: Format(time.Now().AddDate(0, 0, -7))},
+		{name: "Month", t: time.Now().AddDate(0, 1, 0), want: Format(time.Now().AddDate(0, 1, 0))},
+		{name: "MonthAgo", t: time.Now().AddDate(0, -1, 0), want: Format(time.Now().AddDate(0, -1, 0))},
+		{name: "Year", t: time.Now().AddDate(50, 0, 0), want: Format(time.Now().AddDate(50, 0, 0))},
+		{name: "YearAgo", t: time.Now().AddDate(-2, 0, 0), want: Format(time.Now().AddDate(-2, 0, 0))},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotTimeSince := Pretty(tt.t)
+			t.Logf("Pretty() = %v, want %v", gotTimeSince, tt.want)
+			if gotTimeSince != tt.want {
+				t.Errorf("Pretty() = %v, want %v", gotTimeSince, tt.want)
+			}
+		})
+	}
+}
+
+func ExamplePretty() {
+	timeSlots := []struct {
+		name string
+		t    time.Time
+	}{
+		{name: "Just now", t: time.Now()},
+		{name: "Second", t: time.Now().Add(
+			time.Hour*time.Duration(0) +
+				time.Minute*time.Duration(0) +
+				time.Second*time.Duration(1)),
+		},
+		{name: "SecondAgo", t: time.Now().Add(
+			time.Hour*time.Duration(0) +
+				time.Minute*time.Duration(0) +
+				time.Second*time.Duration(-1)),
+		},
+		{name: "Minutes", t: time.Now().Add(time.Hour*time.Duration(0) +
+			time.Minute*time.Duration(59) +
+			time.Second*time.Duration(59))},
+		{name: "Tomorrow", t: time.Now().AddDate(0, 0, 1)},
+		{name: "Yesterday", t: time.Now().AddDate(0, 0, -1)},
+		{name: "Week", t: time.Now().AddDate(0, 0, 7)},
+		{name: "WeekAgo", t: time.Now().AddDate(0, 0, -7)},
+		{name: "Month", t: time.Now().AddDate(0, 1, 0)},
+		{name: "MonthAgo", t: time.Now().AddDate(0, -1, 0)},
+		{name: "Year", t: time.Now().AddDate(2, 0, 0)},
+		{name: "YearAgo", t: time.Now().AddDate(-2, 0, 0)},
+	}
+
+	for _, timeSlot := range timeSlots {
+		fmt.Printf("%s = %v\n", timeSlot.name, Format(timeSlot.t))
+	}
 }
